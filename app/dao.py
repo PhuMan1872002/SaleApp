84a5777916bf98b8738a9ad16b670c6bd5d73903
PhuMan1872002 <phuman1872002@gmail.com>
2023-11-19 19:57:53 +0700

    Page list and log in

--- app/dao.py
+++ app/dao.py
@@ -1,52 +1,38 @@
+from app import login,app
+from app.models import Category,Product,User
+import hashlib
 def load_categories():
-    return[{
-        'id':1,
-        'name':'Mobile'
-    },
-        {
-            'id': 2,
-            'name': 'Tablet'
-        }
-    ]
-def load_products(kw=None):
-    products = [{
-        'id': 1,
-        'name': 'Mobile',
-        'price': 2000000,
-        'image':'https://cdn.tgdd.vn/Products/Images/42/289700/iphone-14-pro-max-bac-thumb-600x600.jpg'
-    },
-        {
-            'id': 2,
-            'name': 'iphone',
-            'price': 2000000,
-            'image': 'https://cdn.tgdd.vn/Products/Images/42/289700/iphone-14-pro-max-bac-thumb-600x600.jpg'
-        },
-        {
-            'id': 3,
-            'name': 'iphone',
-            'price': 2000000,
-            'image': 'https://cdn.tgdd.vn/Products/Images/42/289700/iphone-14-pro-max-bac-thumb-600x600.jpg'
-        },
-        {
-            'id': 4,
-            'name': 'iphone',
-            'price': 2000000,
-            'image': 'https://cdn.tgdd.vn/Products/Images/42/289700/iphone-14-pro-max-bac-thumb-600x600.jpg'
-        },
-        {
-            'id': 5,
-            'name': 'iphone',
-            'price': 2000000,
-            'image': 'https://cdn.tgdd.vn/Products/Images/42/289700/iphone-14-pro-max-bac-thumb-600x600.jpg'
-        },
-        {
-            'id': 6,
-            'name': 'iphone',
-            'price': 2000000,
-            'image': 'https://cdn.tgdd.vn/Products/Images/42/289700/iphone-14-pro-max-bac-thumb-600x600.jpg'
-        }
-    ]
+    return Category.query.all()
+
+
+def load_products(kw=None,cate_id=None,page=None):
+    products=Product.query
 
     if kw:
-        products = [p for p in products if p['name'].find(kw)>=0]
-    return products
+        products=products.filter(Product.name.contains(kw))
+
+    if cate_id:
+        products = products.filter(Product.category_id.__eq__(cate_id))
+
+    if page:
+        page = int(page)
+        page_size = app.config['PAGE_SIZE']
+        start = (page - 1)*page_size
+
+        return products.slice(start, start + page_size)
+
+
+    return products.all()
+
+def count_product():
+    return Product.query.count()
+
+
+def auth_user(username, password):
+    password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+
+    return User.query.filter(User.username.__eq__(username.strip()),
+                             User.password.__eq__(password)).first()
+
+def get_user_by_id(id):
+    return User.Query.get(id)
--- app/index.py
+++ app/index.py
@@ -1,17 +1,52 @@
-from flask import Flask,render_template, request
-import dao
+import math
 
-app=Flask(__name__)
+from flask import Flask, render_template, request, redirect
+import dao
+from app import app,login
+from flask_login import login_user,logout_user
+from app.models import Category, Product, User
+
+
+
 
 @app.route('/')
 def index():
     kw= request.args.get('kw')
     cates = dao.load_categories()
-    product = dao.load_products(kw=kw)
+    cate_id=request.args.get('cate_id')
+    page=request.args.get('page')
+    product = dao.load_products(kw=kw,cate_id=cate_id,page=page)
 
-    return render_template('index.html',categories=cates, products=product)
+    num = dao.count_product()
+
+    return render_template('index.html', categories=cates, products=product,
+                           pages=math.ceil(num/app.config['PAGE_SIZE']))
 @app.route('/products/<id>')
 def details(id):
     return render_template('detail.html')
+
+# @app.route('/admin/login',methods=['post'])
+# def login_admin_process():
+#     request.form.get('name')
+#     request.form.get('passwd')
+@app.route('/admin/login',methods=['post'])
+def login_admin():
+    username = request.form.get('username')
+    password = request.form.get('pwd')
+
+    u = dao.auth_user(username=username, password=password)
+    if u:
+        login_user(user=u)
+
+    return redirect('/admin')
+
+
+@login.user_loader
+def get_user_by_id(user_id):
+    return User.query.get(user_id)
+
+
+
 if __name__=='__main__':
+    from app import admin
     app.run(debug=True)
